52ab424c977610d95f3cc56395390af4111403fa
nojusang <jswo3901@gmail.com>
2017-04-29 08:47:48 +0900

    1.0.15

--- src/socket_handles/login.js
+++ src/socket_handles/login.js
@@ -1,21 +1,41 @@
-  import { MongoClient } from 'mongodb';
+import { MongoClient } from 'mongodb';
 import md5 from 'md5';
 import { emitObj } from '../socket_handle';
 
 const login_handle = (socket, data) => {
-  if ( typeof process.env.MONGOURI == 'undefined')
+  let username = data.name.toLowerCase ();
+
+  if ( typeof process.env.MONGOURI == 'undefined' )
     return emitObj (socket, 'login', {
-      server_error: "Environment variable MONGOURI Not Found"
+      server_error: 'Environment variable MONGOURI not found'
     }, console.error);
 
     MongoClient.connect (process.env.MONGOURI, (err, db) => {
       if ( err )
-      return emitObj (socket, 'signup', {
-        server_error: 'MongoDB connect failed. Description: ' + err.message
-      }, console.warn)
+        return emitObj (socket, 'login', {
+          server_error: 'MongoDB connect failed. Description: ' + err.message
+        }, console.warn)
 
-      db.clse();
+      db.collection ('vote_users').findOne ({
+        hash: md5 (username + ':' + data.pass)
+      }, (err, doc) => {
+        if (err)
+          return emitObj (socket, 'login', {
+            server_error: 'MongoDB findOne error. Description: ' + err.message,
+            $close_db: db
+          }, console.warn);
+
+        if ( !doc )
+          return emitObj (socket, 'login', {
+            error: 'Username or password invalid.',
+            $close_db: db
+          });
+
+        /* TODO: init session / update login\logged in database ( findOne -> findAndModify? ) */
+
+        emitObj (socket, 'login', { error: null, $close_db: db });
+      });
     });
-  };
+};
 
-export deafult login_handle;
+export default login_handle;
